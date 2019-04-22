cd33f50292e1a2b40eb29db7ae76a984b2b8fbd3
Oozaku <willian.ozako@gmail.com>
2019-04-22 14:56:02 -0300

    Definindo interfaces

---
+++ doutorZumbi.java
@@ -0,0 +1,15 @@
+// -------------Usado pelo paciente---------------
+public interface IResponder{
+  public String ask(String question);
+  public boolean finalAnswer(String answer);
+}
+
+// -------------Usado pelo doutor-----------------
+public interface IResponderReceptacle{
+  public void connect(IResponder responder);
+}
+
+public interface IEnquirer{
+  public void startInterview()
+}
+//-------------------------------------------------
